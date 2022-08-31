9fc0e2217e77b4e0e15764084880c9d90bbe3a28
Camilo Duran <camilo.duran@globant.com>
2022-08-30 20:48:52 -0400

    update version 1.0.3

--- src/test/java/util/Driver.java
+++ src/test/java/util/Driver.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class Driver {
     //object to class WebDriver
     private static WebDriver driver;
